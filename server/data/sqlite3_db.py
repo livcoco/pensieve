d241b9a865645abce0d2e7e638dc0e840594dc6e
stuart <asdfstuart@gmail.com>
2018-12-18 19:41:45 -0700

    added categories table and used categoryId in preNodes table

--- server/data/sqlite3_db.py
+++ server/data/sqlite3_db.py
@@ -23,21 +23,25 @@ class DatabaseInterface:
         #audioEdit = 'id INTEGER PRIMARY KEY UNIQUE NOT NULL, noteText BLOB'
         self.columnSpecs = {
             'nodes'   : 'nodeId INTEGER, pathRev INTEGER, name BLOB, dx INTEGER, dy INTEGER, lastPathRev INTEGER, PRIMARY KEY (nodeId, pathRev)',
-            'preNodes': 'nodeId INTEGER, pathRev INTEGER, preNodeId INTEGER, prePathRev INTEGER, category BLOB, nameAlias BLOB, PRIMARY KEY (nodeID, pathRev, preNodeId, prePathRev)',
+            'preNodes': 'nodeId INTEGER, pathRev INTEGER, preNodeId INTEGER, prePathRev INTEGER, categoryID INTEGER, nameAlias BLOB, PRIMARY KEY (nodeID, pathRev, preNodeId, prePathRev)',
+            'categories': 'categoryId INTEGER, preNodeId INTEGER, catName BLOB, PRIMARY KEY(categoryId, preNodeId)',
         #    'newIdeas': 'id INTEGER PRIMARY KEY AUTOINCREMENT, noteText BLOB, date BLOB, owner BLOB',
         #    'toDo'    : 'id INTEGER PRIMARY KEY AUTOINCREMENT, noteText BLOB, date BLOB, owner BLOB, completeByDate BLOB'
         }
         self.sqlAdds = {
             'nodes'   : 'INSERT INTO nodes (nodeId, pathRev, name, dx, dy, lastPathRev) VALUES (?, ?, ?, ?, ?, ?)',
-            'preNodes': 'INSERT INTO preNodes (nodeId, pathRev, preNodeId, prePathRev, category, nameAlias) VALUES (?, ?, ?, ?, ?, ?)',
+            'preNodes': 'INSERT INTO preNodes (nodeId, pathRev, preNodeId, prePathRev, categoryID, nameAlias) VALUES (?, ?, ?, ?, ?, ?)',
+            'categories':'INSERT INTO categories (categoryId, preNodeId, catName) VALUES (?, ?, ?)',
         }
         self.sqlDumps = {
             'nodes' : 'SELECT nodeId, pathRev, name, dx, dy, lastPathRev from nodes',
-            'preNodes' : 'SELECT nodeId, pathRev, preNodeId, prePathRev, category, nameAlias from preNodes',
+            'preNodes' : 'SELECT nodeId, pathRev, preNodeId, prePathRev, categoryID, nameAlias from preNodes',
+            'categories' : 'SELECT categoryId, preNodeId, catName from categories',
         }
         self.tableInits = {
             'nodes'   : (0, 0, None, None, None, None),
             'preNodes'   : (0, 0, None, None, None, None),
+            'categories' : (0, None, None),
         }
         if create_new_database:
             for tableName in self.columnSpecs.keys():
@@ -49,10 +53,110 @@ class DatabaseInterface:
                     assert False, 'I was asked to create a new database, but there is already a database with something in it'
                 except sqlite3.OperationalError:
                     self._addTable(tableName)
-                    
-    def addNodes(self, nodes, category, pre_node_name = None, pre_node_id = None):
+
+    def addNodesToCategory(self, nodes, category_name = None, category_id = None, pre_node_name = None, pre_node_id = None):
         '''
-        add the nodes to the table nodes
+        do not create a new category, reuse an existing category, just extend it with new nodes
+        '''
+        # get the category id and pass it to addNodes which will know to add to the category since it got the category id
+        if category_id != None:
+            categoryId = category_id
+        else:
+            cursor = self.dbCursor.execute('SELECT categoryID FROM categories WHERE catName == \"' + str(category_name) + '\"')
+            data = cursor.fetchone()
+            if data:
+                (categoryId,) = data
+            else:
+                return False
+            data2 = cursor.fetchone()
+            if data2:
+                # there are two different categories with the same name.  The user will need to figure out which one he wants
+                raise AmbiguousNameException()
+        self.addNodes(nodes, category_name, categoryId, pre_node_name, pre_node_id)
+        
+    def addNodes(self, nodes, category_name = None, category_id = None, pre_node_name = None, pre_node_id = None):
+        '''
+        add the nodes to the table nodes, add information to preNodes, and if the category is new, add a new category
+        nodes: a list of nodes to be added to the nodes table and the preNodes table
+        category: the category for the preNodes table
+        pre_node_name, pre_node_id:
+          None, None: No preNode is specified, which is OK, just leave preNode data blank
+          'name', None: a name is specified, but no nodeId, so look up the nodeId corresponding to the name
+          None, nodeId: the nodeId is specified, so use that directly
+          'name', nodeId: both are specified, validate the name matches the nodeId
+        raises AmbiguousNameException if the category_name or pre_node_name is given without its corresponding id, and there are more than one id with the same name.
+        '''
+        show = False
+        if show: print('in addNodes() with nodes', nodes, ', category_name', category_name, ', category_id', category_id, ', pre_node_name', pre_node_name, ', pre_node_id', pre_node_id)
+
+        # get the max nodeId and pathRev so we can add new nodes with unique ids and assign them a new pathRev
+        nodesColumnSpec = self.columnSpecs['nodes']
+        entries = nodesColumnSpec.split()
+        nodesPrimaryKey1 = entries[-2].replace('(','').replace(',','')
+        nodesPrimaryKey2 = entries[-1].replace(')','').replace(',','')
+        if show: print('  nodes table primary keys:', nodesPrimaryKey1, nodesPrimaryKey2)
+        cursor = self.dbCursor.execute('SELECT MAX(' + nodesPrimaryKey1 + ') FROM nodes')
+        (nodeId,) = cursor.fetchone()
+        nodeId += 1
+        cursor = self.dbCursor.execute('SELECT MAX(' + nodesPrimaryKey2 + ') FROM nodes')
+        (pathRev,) = cursor.fetchone()
+        pathRev += 1
+            
+        # find the preNodeId if possible or raise an AmbiguousNameException
+        lastPathRev = None
+        if pre_node_name == None and pre_node_id == None:
+            preNodeId = None
+            prePathRev = None
+        elif pre_node_id != None:
+            preNodeId = pre_node_id
+            cursor = self.dbCursor.execute('SELECT MAX(pathRev) FROM nodes WHERE nodeId == \"' + str(pre_node_id) + '\"')
+            (prePathRev,) = cursor.fetchone()
+        else:
+            #find the nodeId and pathRev for the pre_node_name
+            cursor = self.dbCursor.execute('SELECT nodeId, pathRev, lastPathRev FROM nodes WHERE name == \"' + pre_node_name + '\"')
+            preNodeId, prePathRev = -1, -1
+            while True:
+                # bug: does not handle two entries with the same name, but different nodeIds
+                data = cursor.fetchone()
+                if data == None:
+                    break
+                (tmpPreNodeId, tmpPrePathRev, lastPathRev) = data
+                if lastPathRev:
+                    continue
+                if tmpPrePathRev > prePathRev:
+                    # get the latest revision of pre_node_name
+                    preNodeId, prePathRev = tmpPreNodeId, tmpPrePathRev
+                    
+        # set the category id, either by using and exsisting id (add new nodes to the category) or creating a new category
+        if category_name == None and category_id == None:
+            categoryId = None
+        elif category_id != None:
+            # do not create a new category, instead add new nodes to an existing category
+            categoryId = category_id
+        elif category_name != None:
+            # make a new category
+            cursor = self.dbCursor.execute('SELECT MAX(categoryId) FROM categories')
+            (categoryId,) = cursor.fetchone()
+            categoryId += 1
+            if show: print('  added new category: (categoryId, preNodeId, category_name)', (categoryId, preNodeId, category_name))
+            self.dbCursor.execute(self.sqlAdds['categories'], (categoryId, preNodeId, category_name))
+
+        # add the nodes to both the nodes table and the preNodes table
+        dx, dy = 1, 0
+        for node in nodes:
+            self.dbCursor.execute(self.sqlAdds['nodes'], (nodeId, pathRev, node, dx, dy, lastPathRev))
+            self.dbCursor.execute(self.sqlAdds['preNodes'], (nodeId, pathRev, preNodeId, prePathRev, categoryId, node))
+            if show:
+                print('    added to nodes:', (nodeId, pathRev, node, dx, dy, lastPathRev))
+                print('    added to preNodes:', (nodeId, pathRev, preNodeId, prePathRev, categoryId, node))
+                
+            nodeId += 1
+            dy += 1
+        self.db.commit()
+
+    def OLDaddNodes(self, nodes, category, pre_node_name = None, pre_node_id = None):
+        '''
+        add the nodes to the table nodes, add information to preNodes, and if the category is new, add a new category
         nodes: a list of nodes to be added to the nodes table and the preNodes table
         category: the category for the preNodes table
         pre_node_name, pre_node_id:
@@ -218,6 +322,8 @@ class DatabaseInterface:
     
     def findNotes(self, table_name):
         pass
+
+class AmbiguousNameException(BaseException): pass
 
 if __name__ == '__main__':
 #    path = '/home/stuart/code/button-grid/button-grid/database/test.db'
--- test/data_storage_path_test.py
+++ test/data_storage_path_test.py
@@ -28,9 +28,13 @@ class DataStoragePathTest(unittest.TestCase):
         expPreNodesTuples = (
             (0, 0, None, None, None, None),
             (1, 1, None, None, None, 'farm'),
-            (2, 2, 1, 1, 'animals', 'horse'),
-            (3, 2, 1, 1, 'animals', 'pig'),
-            (4, 2, 1, 1, 'animals', 'dog'),
+            (2, 2, 1, 1, 1, 'horse'),
+            (3, 2, 1, 1, 1, 'pig'),
+            (4, 2, 1, 1, 1, 'dog'),
+        )
+        expCategoriesTuples = (
+            (0, None, None),
+            (1, 1, 'animals')
         )
         path = './test.db'
         lock = multiprocessing.Lock()
@@ -39,7 +43,7 @@ class DataStoragePathTest(unittest.TestCase):
         nodes = ('horse', 'pig', 'dog')
         preNode = 'farm'
         db.addNodes((preNode,), None, None)
-        db.addNodes(nodes, category, preNode)
+        db.addNodes(nodes, category_name = category, pre_node_name = preNode)
         nodesTuples = db.dumpTable('nodes')
         if show:
             print('    nodesTuples:', end=' ')
@@ -50,6 +54,7 @@ class DataStoragePathTest(unittest.TestCase):
                 print('     ', nodesTuple)
         else:
             self.assertEqual(expNodesTuples, nodesTuples)
+
         preNodesTuples = db.dumpTable('preNodes')
         if show:
             print('    preNodesTuples:', end=' ')
@@ -60,7 +65,18 @@ class DataStoragePathTest(unittest.TestCase):
                 print('     ', preNodesTuple)
         else:
             self.assertEqual(expPreNodesTuples, preNodesTuples)
-            
+
+        actCategoriesTuples = db.dumpTable('categories')
+        if show:
+            print('    categoriesTuples:', end=' ')
+            if expCategoriesTuples != actCategoriesTuples:
+                print('******************************** ERROR ****************************')
+            else: print()
+            for categoriesTuple in actCategoriesTuples:
+                print('     ', categoriesTuple)
+        else:
+            self.assertEqual(expCategoriesTuples, actCategoriesTuples)
+        
     def test_03_getNodeIds(self):
         show = False
         if show: print('  test_03_getNodeIds')
@@ -94,13 +110,18 @@ class DataStoragePathTest(unittest.TestCase):
         expPreNodesTuples = (
             (0, 0, None, None, None, None),
             (1, 1, None, None, None, 'farm'),
-            (2, 2, 1, 1, 'animals', 'horse'),
-            (3, 2, 1, 1, 'animals', 'pig'),
-            (4, 2, 1, 1, 'animals', 'dog'),
-            (5, 3, 2, 2, 'Horse Breeds', 'Quarter horse'),
-            (6, 3, 2, 2, 'Horse Breeds', 'Mustang'),
-            (7, 3, 2, 2, 'Horse Breeds', 'Appaloosa'),
-            (8, 3, 2, 2, 'Horse Breeds', 'Morgan horse'),
+            (2, 2, 1, 1, 1, 'horse'),
+            (3, 2, 1, 1, 1, 'pig'),
+            (4, 2, 1, 1, 1, 'dog'),
+            (5, 3, 2, 2, 2, 'Quarter horse'),
+            (6, 3, 2, 2, 2, 'Mustang'),
+            (7, 3, 2, 2, 2, 'Appaloosa'),
+            (8, 3, 2, 2, 2, 'Morgan horse'),
+        )
+        expCategoriesTuples = (
+            (0, None, None),
+            (1, 1, 'animals'),
+            (2, 2, 'Horse Breeds'),
         )
         path = './test.db'
         lock = multiprocessing.Lock()
@@ -131,5 +152,65 @@ class DataStoragePathTest(unittest.TestCase):
         else:
             self.assertEqual(expPreNodesTuples, preNodesTuples)
 
+    def test_05_addNodesToCategory(self):
+        show = True
+        if show: print('  test_05_addNodesToCategory')
+        expNodesTuples = (
+            (0, 0, None, None, None, None),
+            (1, 1, 'farm', 1, 0, None),
+            (2, 2, 'horse', 1, 0, None),
+            (3, 2, 'pig', 1, 1, None),
+            (4, 2, 'dog', 1, 2, None),
+            (5, 3, 'Quarter horse', 1, 0, None),
+            (6, 3, 'Mustang', 1, 1, None),
+            (7, 3, 'Appaloosa', 1, 2, None),
+            (8, 3, 'Morgan horse', 1, 3, None),
+            (9, 4, 'sheep', 1, 0, None),
+        )
+        expPreNodesTuples = (
+            (0, 0, None, None, None, None),
+            (1, 1, None, None, None, 'farm'),
+            (2, 2, 1, 1, 1, 'horse'),
+            (3, 2, 1, 1, 1, 'pig'),
+            (4, 2, 1, 1, 1, 'dog'),
+            (5, 3, 2, 2, 2, 'Quarter horse'),
+            (6, 3, 2, 2, 2, 'Mustang'),
+            (7, 3, 2, 2, 2, 'Appaloosa'),
+            (8, 3, 2, 2, 2, 'Morgan horse'),
+            (9, 4, 1, 1, 1, 'sheep')
+        )
+        expCategoriesTuples = (
+            (0, None, None),
+            (1, 1, 'animals'),
+            (2, 2, 'Horse Breeds'),
+        )
+        path = './test.db'
+        lock = multiprocessing.Lock()
+        db = DatabaseInterface(path, lock)
+        category = 'animals'
+        nodes = ('sheep',)
+        preNode = 'farm'
+        db.addNodesToCategory(nodes, category_name = category, pre_node_name = preNode)
+
+        actNodesTuples = db.dumpTable('nodes')
+        self.compareTuples('nodes', expNodesTuples, actNodesTuples, show)
+
+        actPreNodesTuples = db.dumpTable('preNodes')
+        self.compareTuples('nodes', expPreNodesTuples, actPreNodesTuples, show)
+
+        actCategoriesTuples = db.dumpTable('categories')
+        self.compareTuples('nodes', expCategoriesTuples, actCategoriesTuples, show)
+        
+    def compareTuples(self, tuplesName, expTuples, actTuples, show):
+        if show:
+            print('  ', tuplesName,':', end=' ')
+            if expTuples != actTuples:
+                print('******************************** ERROR ****************************')
+            else: print()
+            for actTuple in actTuples:
+                print('     ', actTuple)
+        else:
+            self.assertEqual(expTuples, actTuples)
+        
 if __name__ == '__main__':
     unittest.main()
